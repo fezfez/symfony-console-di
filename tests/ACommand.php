3009991fc43e6f09bf59bd764e7584d6b4b7b2d0
Stéphane Demonchaux <demonchaux.stephane@gmail.com>
2020-12-13 16:14:01 +0100

    fix test

--- tests/ACommand.php
+++ tests/ACommand.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Fezfez\SymfonyDiConsole\Tests;
 
 use Fezfez\SymfonyDiConsole\Command;
+use RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+
+use function is_string;
 
 class ACommand implements Command
 {
@@ -19,8 +22,30 @@ class ACommand implements Command
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->write($this->dependency . 'hola' . $input->getArgument('hi') . $input->getOption('anoption'));
+        $output->write($this->dependency . 'hola' . $this->getArgumentAsString($input, 'hi') . $this->getOptiontAsString($input, 'anoption'));
 
         return 1;
+    }
+
+    private function getArgumentAsString(InputInterface $input, string $name): string
+    {
+        $arg = $input->getArgument($name);
+
+        if (! is_string($arg)) {
+            throw new RuntimeException();
+        }
+
+        return $arg;
+    }
+
+    private function getOptiontAsString(InputInterface $input, string $name): string
+    {
+        $arg = $input->getOption($name);
+
+        if (! is_string($arg)) {
+            throw new RuntimeException();
+        }
+
+        return $arg;
     }
 }
